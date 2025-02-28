61c1273a512d60a96c2fe2aec873a204f2362865
mooha1999 <developer.ahmed99@gmail.com>
2025-02-28 23:23:51 +0200

    Set basename for BrowserRouter to '/GP-YH' for improved routing context

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/GP-YH">
     <App />
   </BrowserRouter>
 );
