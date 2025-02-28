8e11d0a3b347f2921b045ee78df981292884507a
mooha1999 <developer.ahmed99@gmail.com>
2025-02-28 23:24:23 +0200

    Add navigation links and routes for Budget Tracking and App 2 components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import "./App.css";
 import BudgetTracking from "./pages/budget-tracking";
+import App2 from "./pages/app-2";
+import { Route, Link, Routes } from "react-router";
 
 function App() {
-  return <BudgetTracking />;
+  return (
+    <div className="app">
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/budget-tracking" className="nav-link">
+              Budget Tracking
+            </Link>
+          </li>
+          <li>
+            <Link to="/app-2" className="nav-link">
+              App 2
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/budget-tracking" element={<BudgetTracking />} />
+        <Route path="/app-2" element={<App2 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
