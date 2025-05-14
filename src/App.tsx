dda1a5d163fc23fde481263ad0c6673e891bf1fe
mooha1999 <developer.ahmed99@gmail.com>
2025-05-15 02:53:13 +0300

    feat: add App4 navigation and routing to the main application

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetTracking from "./pages/budget-tracking";
 import App2 from "./pages/app-2";
 import App3 from "./pages/app-3";
 import { Route, Link, Routes } from "react-router";
+import App4 from "./pages/app-4";
 
 function App() {
   return (
@@ -24,12 +25,18 @@ function App() {
               App 3
             </Link>
           </li>
+          <li>
+            <Link to="/app-4" className="nav-link">
+              App 4
+            </Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/budget-tracking" element={<BudgetTracking />} />
         <Route path="/app-2" element={<App2 />} />
         <Route path="/app-3" element={<App3 />} />
+        <Route path="/app-4" element={<App4 />} />
       </Routes>
     </div>
   );
